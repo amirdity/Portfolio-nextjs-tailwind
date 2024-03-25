12fb33af8a4d5f2c01d0a633303f1bd6ca67e608
amir fattahi <amirfattahi22@gmail.com>
2024-03-25 23:14:21 +0330

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Layout from "./Layout";
 
 const Footer = () => {  
-  return ( 
+  return (  
     <footer className="w-full border-t-1 border-solid border-dark font-medium text-lg  dark:text-light">
       <Layout classname="py-8 flex items-center justify-between">
         <span>{new Date().getFullYear()} © All Rights Reserved.</span>
